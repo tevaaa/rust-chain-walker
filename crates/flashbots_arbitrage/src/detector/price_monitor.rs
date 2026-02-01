9c3afe81394cd35bbca7b03ef6b76cd31b995b84
tevaaa <andretevaa@gmail.com>
2026-02-01 01:05:29 +0100

    I will work with v2

--- crates/flashbots_arbitrage/src/detector/price_monitor.rs
+++ crates/flashbots_arbitrage/src/detector/price_monitor.rs
@@ -5,9 +5,9 @@ use serde_json::json;
 use tokio::select;
 use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
 
-const UNI_V3_POOL: &str = "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640";
-const SUSHI_V3_POOL: &str = "0xf3Eb87C1F6020982173C908E7eB31aA66c1f0296";
-const SWAP_EVENT: &str = "0xc42021b1a9404827050d212870743a014596206b006a619c961917f69906646b";
+const UNI_V2_POOL: &str = "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc";
+const SUSHI_V2_POOL: &str = "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0";
+const SWAP_EVENT: &str = "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822";
 
 pub struct PriceMonitor {
     uni_reserves: (u128, u128),
@@ -25,40 +25,27 @@ impl PriceMonitor {
     pub async fn handle_event(&mut self, source: &str, msg: Message) -> Result<()> {
         let now = chrono::Local::now();
         let timestamp = now.format("%H:%M:%S%.3f").to_string();
-        println!("{} Received {}", timestamp, source);
 
-        let raw_text = match msg {
-            Message::Text(t) => t,
-            Message::Binary(b) => String::from_utf8_lossy(&b).to_string(),
-            Message::Ping(_) => {
-                println!("PING");
-                // println!("{} [PING] from {}", timestamp, source); // Trop de bruit
-                return Ok(());
-            }
-            Message::Pong(_) => return Ok(()),
-            _ => return Ok(()),
-        };
-        // if let Message::Text(text) = msg {
-        // Desrialise
-        let v: serde_json::Value =
-            serde_json::from_str(&raw_text).context("Failed to parse JSON-RPC message")?;
-        println!("{}", v);
+        if let Message::Text(text) = msg {
+            // Desrialise
+            println!("{} Received {} Method: {}", timestamp, source, v["method"]);
+            let v: serde_json::Value =
+                serde_json::from_str(&text).context("Failed to parse JSON-RPC message")?;
 
-        // Parser
-        if v["method"] == "eth_subscription" {
-            if let Some(data_hex) = v["params"]["result"]["data"].as_str() {
-                let amounts = parser::parse_swap_log(data_hex);
-                self.update_price(source, amounts).await;
+            // Parser
+            if v["method"] == "eth_subscription" {
+                if let Some(data_hex) = v["params"]["result"]["data"].as_str() {
+                    let amounts = parser::parse_swap_log(data_hex);
+                    self.update_price(source, amounts).await;
+                }
+            } else if v["result"].is_string() {
+                println!("Subscription confirmed for {}", source);
             }
-        } else if v["result"].is_string() {
-            println!("Subscription confirmed for {}", source);
         }
-        //}
         Ok(())
     }
 
     pub async fn update_price(&mut self, source: &str, amounts: parser::SwapAmounts) {
-        println!("You are here");
         let in_0 = amounts.amount0_in as f64 / 1_000_000.0;
         let in_1 = amounts.amount1_in as f64 / 1_000_000_000_000_000_000.0;
         let out_0 = amounts.amount0_out as f64 / 1_000_000.0;
@@ -88,13 +75,13 @@ impl PriceMonitor {
         // Subscribe to contracts
         let sub_uni = json!({
                 "jsonrpc": "2.0", "id": 1, "method": "eth_subscribe",
-                "params": ["logs", {"address": UNI_V3_POOL, "topics": [SWAP_EVENT]}]
+                "params": ["logs", {"address": UNI_V2_POOL, "topics": [SWAP_EVENT]}]
         });
         uni_write.send(Message::Text(sub_uni.to_string())).await?;
 
         let sub_sushi = json!({
                 "jsonrpc": "2.0", "id": 2, "method": "eth_subscribe",
-                "params": ["logs", {"address": SUSHI_V3_POOL, "topics": [SWAP_EVENT]}]
+                "params": ["logs", {"address": SUSHI_V2_POOL, "topics": [SWAP_EVENT]}]
         });
         sushi_write
             .send(Message::Text(sub_sushi.to_string()))
